3f4b6d6f7ec8f7c23cf38bd4945f83d44bad6cc5
hi20160616 <hi20160616@163.com>
2022-07-15 00:39:29 -0400

    fix powermenu.

--- dot_config/polybar/scripts/lock.sh
+++ dot_config/polybar/scripts/lock.sh
@@ -1,63 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 
-blank='#00000000'  # blank
-#background='#1A6785FF'
-background='#1A5E81AC'
-foreground='#D8DEE9FF'  # text
-
-primary='#82b1ff'  # default
-alert='#EBCB8BFF'  # wrong
-verifying='#A3BE8CFF'  # verifying
-
-sleep 0.3
+BLANK='#3c3836'
+CLEAR='#7c6f64'
+DEFAULT='#928374'
+TEXT='#ebdbb2'
+WRONG='#cc241d'
+VERIFYING='#a89984'
 
 i3lock \
---screen 1 \
---indicator \
---keylayout 2 \
---force-clock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
 \
---insidevercolor=$background    \
---insidewrongcolor=$background  \
---insidecolor=$background       \
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
 \
---ringvercolor=$primary         \
---ringwrongcolor=$alert         \
---ringcolor=$primary            \
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
 \
---linecolor=$primary            \
---line-uses-inside \
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
 \
---keyhlcolor=$alert             \
---bshlcolor=$alert              \
-\
---separatorcolor=$primary       \
-\
---verifcolor=$foreground        \
---wrongcolor=$foreground        \
---timecolor=$foreground \
---datecolor=$foreground         \
---layoutcolor=$foreground       \
-\
---time-align=0 \
---date-align=0 \
---layout-align=0 \
---verif-align=0 \
---wrong-align=0 \
---modif-align=0 \
-\
---blur 5              \
---indicator           \
---keylayout 2         \
-\
---veriftext=""          \
---wrongtext=""          \
---clock               \
---timestr="%H:%M:%S"  \
---datestr="%A, %m %Y" \
---modsize=10 \
---time-font=noto-sans    \
---date-font=noto-sans    \
-# etc
-
-
+--screen 1                   \
+--blur 5                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"       \
+--keylayout 1                \
